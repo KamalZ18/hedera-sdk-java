8ded885ce7265ece0eaeaff178f1377e0ef48b23
Kamal <40741000+KamalZ18@users.noreply.github.com>
2019-07-19 21:17:08 +0100

    Update ExampleHelper.java
    19 july 2019: updated by Kamal Zada to make the Java SDK Android (Java v:8) compatible.

--- examples/src/main/java/com/hedera/hashgraph/sdk/examples/ExampleHelper.java
+++ examples/src/main/java/com/hedera/hashgraph/sdk/examples/ExampleHelper.java
@@ -7,59 +7,63 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public final class ExampleHelper {
-    private ExampleHelper() { }
+  /*constructor ExampleHelper*/
+  public ExampleHelper() { }
 
-    private static Dotenv getEnv() {
-        // Load configuration from the environment or a $projectRoot/.env file, if present
-        // See .env.sample for an example of what it is looking for
+  private static Dotenv getEnv() {
+  // Load configuration from the environment or a $projectRoot/.env file, if present
+  // See .env.sample for an example of what it is looking for
         return Dotenv.load();
-    }
+  }/*getEnv*/
 
-    public static AccountId getNodeId() {
+  public static AccountId getNodeId() {
         return AccountId.fromString(Objects.requireNonNull(getEnv().get("NODE_ID")));
-    }
+  }/*getNodeId*/
 
-    public static AccountId getOperatorId() {
+  public static AccountId getOperatorId() {
         return AccountId.fromString(Objects.requireNonNull(getEnv().get("OPERATOR_ID")));
-    }
+  }/*getOperatorId*/
 
-    public static Ed25519PrivateKey getOperatorKey() {
-        return Ed25519PrivateKey.fromString(Objects.requireNonNull(getEnv().get("OPERATOR_KEY")));
-    }
+  public static Ed25519PrivateKey getOperatorKey() {
+      return Ed25519PrivateKey.fromString(Objects.requireNonNull(getEnv().get("OPERATOR_KEY")));
+  }/*getOperatorKey*/
 
-    public static Client createHederaClient() {
-        // To connect to a network with more nodes, add additional entries to the network map
-        var nodeAddress = Objects.requireNonNull(getEnv().get("NODE_ADDRESS"));
-        var client = new Client(Map.of(getNodeId(), nodeAddress));
+  public static Client createHederaClient() {
+  // To connect to a network with more nodes, add additional entries to the network map
+    var nodeAddress = Objects.requireNonNull(getEnv().get("NODE_ADDRESS"));
+    var client = new Client(Map.of(getNodeId(), nodeAddress));
 
-        // Defaults the operator account ID and key such that all generated transactions will be paid for
-        // by this account and be signed by this key
-        client.setOperator(getOperatorId(), getOperatorKey());
+    // Defaults the operator account ID and key such that all generated transactions will be paid for
+    // by this account and be signed by this key
+    client.setOperator(getOperatorId(), getOperatorKey());
 
-        return client;
-    }
+    return client;
+  }/*createHederaClient*/
 
-    public static byte[] parseHex(String hex) {
-        var len = hex.length();
-        var data = new byte[len / 2];
+  public static byte[] parseHex(String hex) {
+    int i, len;
+    byte[] data1;
 
-        int i = 0;
-
-        //noinspection NullableProblems
-        for (var c : (Iterable<Integer>) hex.chars()::iterator) {
-            if ((i % 2) == 0) {
-                // high nibble
-                data[i / 2] = (byte) (Character.digit(c, 16) << 4);
-            } else {
-                // low nibble
-                data[i / 2] &= (byte) Character.digit(c, 16);
-            }
-
-            i++;
+    i = 0;
+    len = hex.length();
+    data1 = new byte[len / 2];
+  
+    //noinspection NullableProblems
+    for (Integer c : (Iterable<Integer>) hex.chars()::iterator) {
+        if ((i % 2) == 0) {
+            // high nibble
+            data1[i / 2] = (byte) (Character.digit(c, 16) << 4);
+        } else {
+            // low nibble
+            data1[i / 2] &= (byte) Character.digit(c, 16);
         }
+      i++;
+    }/*for loop*/
 
-        return data;
-    }
-}
+      return (data1);
+  }/*parseHex*/
+}/*ExampleHelper*/
