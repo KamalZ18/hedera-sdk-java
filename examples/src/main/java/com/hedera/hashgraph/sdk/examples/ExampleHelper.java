f27e247d63b6e238f70e9989cc31083bdbe05751
Kamal <40741000+KamalZ18@users.noreply.github.com>
2019-07-19 21:46:20 +0100

    Update ExampleHelper.java
    19 july 2019: updated by Kamal Zada to make Java SDK, Android and Java v:8 compatible.

--- examples/src/main/java/com/hedera/hashgraph/sdk/examples/ExampleHelper.java
+++ examples/src/main/java/com/hedera/hashgraph/sdk/examples/ExampleHelper.java
@@ -1,5 +1,10 @@
 package com.hedera.hashgraph.sdk.examples;
 
+/*Android SDK classes*/
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+/*Hedera Java SDK classes*/
 import com.hedera.hashgraph.sdk.Client;
 import com.hedera.hashgraph.sdk.account.AccountId;
 import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
@@ -11,17 +16,39 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public final class ExampleHelper {
+  private static final String LogTag = "ExampleHelper";
+
   /*constructor ExampleHelper*/
   public ExampleHelper() { }
 
   private static Dotenv getEnv() {
-  // Load configuration from the environment or a $projectRoot/.env file, if present
-  // See .env.sample for an example of what it is looking for
-        return Dotenv.load();
+  /* The environment file on Android devices is called node.env,
+     Load configuration from the environment --> load from the node.env (.env) file,
+     See .env.sample for an example of what it is looking for.
+     Load configuration from the environment or an node.env file,
+     location of node.env file on Android device: /data/local/tmp
+     the node.env file should contain addresses of 4 nodes.*/
+	  Dotenv dotEnvFile1;
+    
+    /*the call to the directory method sets the location of where to find the node.env file,
+      change this string to where you would like to place your node.env file.*/
+    try {
+    dotEnvFile1 = Dotenv.configure()
+					    .directory("/data/local/tmp")
+              .filename("node.env")
+              .load();
+    } catch (Exception Exp1) {
+      Log.i (LogTag,"getEnv --> Dotenv --> Exception: " + Exp1.toString());
+      Log.i (LogTag,"getEnv --> Dotenv Error: Cannot find node.env file.");
+      Exp1.printStackTrace();
+    }/*try ... catch*/
+    return (dotEnvFile1);
   }/*getEnv*/
 
   public static AccountId getNodeId() {
-        return AccountId.fromString(Objects.requireNonNull(getEnv().get("NODE_ID")));
+    AccountId acountId1;
+    acountId1 = AccountId.fromString(Objects.requireNonNull(getEnv().get("NODE_ID")));
+    return AccountId.fromString(Objects.requireNonNull(getEnv().get("NODE_ID")));
   }/*getNodeId*/
 
   public static AccountId getOperatorId() {
@@ -33,15 +60,21 @@ public final class ExampleHelper {
   }/*getOperatorKey*/
 
   public static Client createHederaClient() {
-  // To connect to a network with more nodes, add additional entries to the network map
-    var nodeAddress = Objects.requireNonNull(getEnv().get("NODE_ADDRESS"));
-    var client = new Client(Map.of(getNodeId(), nodeAddress));
+  /* To connect to a network with more nodes, add additional entries to the network map
+     This method connects to a network with 1 nodes. */
+    Client client1;
+    String nodeAddres1,
+ 
+    nodeAddres1 = Objects.requireNonNull(getEnv().get("NODE_ADDRESS"));
+    
+    /*Create a new Client with 1 node.*/
+    client1 = new Client(Map.of(getNodeId(), nodeAddres1));
 
     // Defaults the operator account ID and key such that all generated transactions will be paid for
     // by this account and be signed by this key
-    client.setOperator(getOperatorId(), getOperatorKey());
+    client1.setOperator(getOperatorId(), getOperatorKey());
 
-    return client;
+    return (client1);
   }/*createHederaClient*/
 
   public static byte[] parseHex(String hex) {
