d865e4eb75e8379d8ef535029b6f38f766aca80c
Kamal <40741000+KamalZ18@users.noreply.github.com>
2019-07-20 14:03:09 +0100

    Update GetAccountBalance.java
    20 july 2019: updated by Kamal Zada to make the Java SDK, Android and Java v:8 compatible.

--- examples/src/main/java/com/hedera/hashgraph/sdk/examples/simple/GetAccountBalance.java
+++ examples/src/main/java/com/hedera/hashgraph/sdk/examples/simple/GetAccountBalance.java
@@ -4,13 +4,18 @@ import com.hedera.hashgraph.sdk.HederaException;
 import com.hedera.hashgraph.sdk.examples.ExampleHelper;
 
 public final class GetAccountBalance {
-    private GetAccountBalance() { }
+  /*Constructor*/
+  private GetAccountBalance() { }
 
-    public static void main(String[] args) throws HederaException {
-        var operatorId = ExampleHelper.getOperatorId();
-        var client = ExampleHelper.createHederaClient();
-        var balance = client.getAccountBalance(operatorId);
+  public static void main(String[] args) throws HederaException {
+    AccountId operatorId1;
+    Client client1;
+    long balance1;
+    
+    operatorId1 = ExampleHelper.getOperatorId();
+    client1 = ExampleHelper.createHederaClient();
+    balance1 = client.getAccountBalance(operatorId);
 
-        System.out.println("balance = " + balance);
-    }
-}
+    System.out.println("Account balance = " + balance);
+  }/*main*/
+}/*GetAccountBalance*/
